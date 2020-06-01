c6718c13c5cb22975e37d634c893ae6d2b0957a0
tomerar <45894448+tomerar@users.noreply.github.com>
2020-06-01 20:23:31 +0300

    add index login signup

---
+++ js/login.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var firebase_init = new FirebaseInit(); 
+    firebase_init.is_login("index.html",null)
+    // firebase_init.is_login(null,null)
+    var login_tools = new LoginTools(); 
+      
+});
+
---
+++ js/login/login_tools.js
@@ -0,0 +1,413 @@
+
+var FirebaseInit = function(){
+    console.log( "FirebaseInit create" );
+    /*
+     * Variables accessible
+     * in the class
+     */
+    this.user = "none";
+    var vars = {};
+ 
+    /*
+     * Can access this.method
+     * inside other methods using
+     * root.method()
+     */
+    var root = this;
+ 
+    /*
+     * Constructor
+     */
+    this.construct = function(){
+        this.init_firebase();
+        this.logout();
+    };
+ 
+
+    this.if_have_user_name_show_me = function (user) {
+        setTimeout(function(){
+            if (user.displayName != null){
+                $("#hello-user").text("שלום "+ user.displayName);
+                $("#hello-user").css("display","block");
+            }
+        },1000);
+    }
+
+
+    this.update_name = function (){
+        setTimeout(function () {
+            var user = firebase.auth().currentUser;
+            console.log(user.displayName)
+            console.log(localStorage.getItem("fullname"))
+            if (user.displayName == null){
+                user.updateProfile({
+                    displayName: localStorage.getItem("fullname")
+                  }).then(function () {
+                    console.log("work update user name");
+                  }).catch(function (error) {
+                    // An error happened.
+                    console.log("error update user name");
+                  });
+            }
+            this.if_have_user_name_show_me(user)
+        },3000);
+    }
+
+    this.init_firebase = function() {
+        var firebaseConfig = {
+            apiKey: "AIzaSyBYa_jrORmAMavHQMAmKAYqNWLz4Ez7Zaw",
+            authDomain: "privatelessons-telhai.firebaseapp.com",
+            databaseURL: "https://privatelessons-telhai.firebaseio.com",
+            projectId: "privatelessons-telhai",
+            storageBucket: "privatelessons-telhai.appspot.com",
+            messagingSenderId: "173953946690",
+            appId: "1:173953946690:web:a57637bfd0b12cf89409db",
+            measurementId: "G-9T7YLMS7X7"
+          };
+        //   Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+            if (!firebase.apps.length) {
+                firebase.initializeApp({});
+            }
+       
+    };
+
+    this.is_login = function name(login_page,logout_page) {
+         /// is login check
+
+         firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                this.user = user
+                console.log(user);
+                add_user_name_on_nav(user);
+                $("#logout-btn").css("display", "block");
+                $("#login-btn").css("display", "none");
+                if (user.email.localeCompare("admin@admin.com") ==0){
+                    window.location.href = "adminpanel.html";
+                }
+                if(login_page!=null){
+                    window.location.href = login_page;
+                }
+                
+            } else {
+                $("#hello-user").css("display","none");
+                $("#logout-btn").css("display", "none");
+                $("#login-btn").css("display", "block");
+                console.log("not loging");
+                // window.location.href = "login.html";
+                if(logout_page!=null){
+                    window.location.href = logout_page;
+                }
+            }
+        });
+    }
+    this.logout = function (){
+        $("#logout-btn").on("click", function () {
+            var user = firebase.auth().currentUser;
+            firebase.auth().signOut().then(function () {
+              console.log("logout successful")
+            }).catch(function (error) {
+              console.log("logout error " + error);
+            });
+          });
+    }
+
+    var add_user_name_on_nav = function(user){
+        if (user.displayName != null){
+            $("#hello-user").text("שלום "+ user.displayName);
+            $("#hello-user").css("display","block");
+        }
+    }
+ 
+ 
+    /*
+     * Pass options when class instantiated
+     */
+    this.construct();
+ 
+};
+ 
+var LoginTools = function(){
+    console.log( "LoginTools create" );
+    /*
+     * Variables accessible
+     * in the class
+     */
+    var vars = {};
+    this.firstTime = false;
+    /*
+     * Can access this.method
+     * inside other methods using
+     * root.method()
+     */
+    var root = this;
+ 
+    /*
+     * Constructor
+     */
+    this.construct = function(){
+       this.login_btn();
+       this.google_btn();
+       this.signup_btn();
+       this.username_password_btn();
+       this.send_email();
+    };
+ 
+    this.login_btn = function () {
+           ///index page
+        $("#login").click(function () {
+            var emailUser = document.getElementById("first-name").value;
+            var passwordUser = document.getElementById("pass").value;
+            var errLogMessage = document.getElementById("login-err");
+
+            const auth = firebase.auth();
+            const flag = auth.signInWithEmailAndPassword(emailUser, passwordUser);
+            flag.catch(function (e) {
+
+                $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+                e.message+" </p>");
+                setTimeout(function(){$("#login-err").empty();},3000) ;
+                console.log(e.message);
+                $("#login-err").css("display", "block");
+                return;
+            });
+            console.log(emailUser);
+            console.log(passwordUser);
+
+        });
+    }
+    this.google_btn = function () {
+        ///login with google
+        $("#google-btn").click(function () {
+            console.log("click on google btn")
+    
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+        });   
+    }
+    this.signup_btn = function () {
+        //sign up  bt on main page
+        $("#signup").click(function () {
+            window.location.href = "signup.html";
+            this.firstTime = true;
+        });
+    }
+    this.username_password_btn = function () {
+        $("#username-password").click(function () {
+            $("#login").css("display", "none");
+            $("#pass").css("display", "none");
+            $("#first-name").css("display", "none");
+            $("#passdiv").css("display", "none");
+            $("#first-namediv").css("display", "none");
+            $("#signup").css("display", "none");
+            $("#resetemail").css("display", "block");
+            $("#resetemaildiv").css("display", "block");
+            $("#send-email").css("display", "block");
+            $("#login-err").css("display", "none");
+            $("#login-err").empty();
+    
+    
+        });
+    }
+    this.send_email =function () {
+        $("#send-email").click(function () {
+            $("#login").css("display", "block");
+            $("#pass").css("display", "block");
+            $("#first-name").css("display", "block");
+            $("#passdiv").css("display", "block");
+            $("#first-namediv").css("display", "block");
+            $("#signup").css("display", "block");
+            $("#resetemail").css("display", "none");
+            $("#resetemaildiv").css("display", "none");
+            $("#send-email").css("display", "none");
+            $("#login-err").css("display", "none");
+            $("#login-err").empty();
+            var auth = firebase.auth();
+            var emailAddress = document.getElementById("resetemail").value
+    
+            console.log(emailAddress);
+    
+            auth.sendPasswordResetEmail(emailAddress).then(function () {
+                
+                $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+                "email sent, check your email </p>");
+                setTimeout(function(){$("#login-err").empty();},3000) ;
+                $("#login-err").css("display", "block");
+            }).catch(function (error) {
+                $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+                error.message+" </p>");
+                setTimeout(function(){$("#login-err").empty();},3000) ;
+                $("#login-err").css("display", "block");
+                console.log(error);
+            });
+        });
+    }
+
+
+
+    /*
+     * Pass options when class instantiated
+     */
+    this.construct();
+ 
+};
+
+var SignUpTools = function(){
+    console.log( "SignUpTools create" );
+    /*
+     * Variables accessible
+     * in the class
+     */
+    var vars = {};
+    
+    /*
+     * Can access this.method
+     * inside other methods using
+     * root.method()
+     */
+    var root = this;
+ 
+    /*
+     * Constructor
+     */
+    this.construct = function(){
+        this.back_login_btn();
+        this.signup_prosses_btn();
+    };
+
+    this.back_login_btn = function () {
+        $("#login2").click(function(){
+            window.location.href = "login.html";
+        });
+    }
+
+    this.signup_prosses_btn = function () {
+      $("#signup2").click(function () {
+        $("#login-err").css("display", "none");
+        var radios = document.getElementsByName('genderS');
+        var gender;
+        for (var i = 0, length = radios.length; i < length; i++) {
+            if (radios[i].checked) {
+                gender = radios[i].value;
+                break;
+            }
+        }
+        var email = $("#first-nameup").val();
+        var password = $("#passup").val();
+        var password2 = $("#pass2up").val();
+        var fullname = $("#fullnameup").val();
+        var bday = $("#bdayup").val();
+        var dateUser = new Date(bday);
+
+
+        var pass="";
+        var repass="";
+         pass = $("#passup").val();
+         repass = $("#pass2").val();
+         console.log(pass);
+         console.log(repass);
+
+        if ((pass.length == 0) || (repass.length == 0)) {
+            $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+           "the password is empty </p>");
+            setTimeout(function(){$("#login-err").empty();},10000) ;
+            $("#login-err").css("display", "block");
+            return;
+        }
+        else if (pass != repass) {
+            $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+            "the password is not the same </p>");
+            setTimeout(function(){$("#login-err").empty();},10000) ;
+            $("#login-err").css("display", "block");
+            return;
+        }
+        else {
+           console.log("pass the some");
+        }
+        if(fullname.length>20){
+            $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+            "full name is too long only 20 Letters </p>");
+            setTimeout(function(){$("#login-err").empty();},10000) ;
+            $("#login-err").css("display", "block");
+            return;
+        }
+        if(fullname.length==0){
+            $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+            "full name is empty</p>");
+            setTimeout(function(){$("#login-err").empty();},10000) ;
+            $("#login-err").css("display", "block");
+            return;
+        }
+
+        if(isDate18orMoreYearsOld(dateUser.getDay(),dateUser.getMonth(),dateUser.getFullYear())==false)
+        {
+            $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+            "you Must be older than 18 </p>");
+            setTimeout(function(){$("#login-err").empty();},10000) ;
+            $("#login-err").css("display", "block");
+            return;
+        }
+
+
+
+
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+        localStorage.setItem("fullname", fullname);
+        localStorage.setItem("bday", bday);
+        localStorage.setItem("gender", gender);
+        localStorage.setItem("signup","yes");
+        firstTime = true;
+       
+
+        var okToMove = true;
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            okToMove = false;
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            $("#login-err").append("<p  class='login100-form-err p-b-15' style='display: block;'>" +
+            error.message+" </p>");
+            setTimeout(function(){$("#login-err").empty();},3000) ;
+            
+           
+            $("#login-err").css("display", "block");
+
+
+        });
+
+        setTimeout(function () {
+
+
+            if (okToMove === true) {
+                
+                console.log("profile");
+                window.location.href = "index.html";
+            }
+        }, 3000)
+    });
+    }
+
+
+    var IsEmail = function (email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (!email.match(re)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    var isDate18orMoreYearsOld = function(day, month, year) {
+        return new Date(year+18, month-1, day) <= new Date();
+    }
+
+
+    /*
+     * Pass options when class instantiated
+     */
+    this.construct();
+ 
+};
+ 
