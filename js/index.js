90c8546880387a1274cb627d1e2ac403f946fa85
tomerar <tomerar5@gmail.com>
2020-06-02 12:47:14 +0300

    try git

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         var firebase_init = new FirebaseInit(); 
         // firebase_init.is_login(null,"login.html")
         firebase_init.is_login(null,"login.html")
+          console.log("wowo");
           
 });
 
