f81058154a66df3ad4c2cec7de96ad503f12f52e
Oren Or <orensmails@gmail.com>
2020-06-05 17:04:08 +0300

    basic profile

---
+++ js/profile.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+	var firebase_init = new FirebaseInit(); 
+	firebase_init.is_login(null,"login.html")
+	var user = firebase.auth().currentUser;
+	firebase.database().ref("user/client/" + user.uid).once('value').then(function (snapshot) {
+		data_client = snapshot.val();
+	}).then(function () {})
+});
+
+
+
