9df13bb471afe2726cbe5e1fcbb90f774595c710
tomerar <tomerar5@gmail.com>
2020-06-14 22:09:48 +0300

    update profile

--- js/profile.js
+++ js/profile.js
@@ -1,29 +1,50 @@
+var database;
+var user_uid;
 $(document).ready(function () {
-   initFireBase();  
+	download_db();  
 });
 
-async function initFireBase(){
+function download_db(){
 	firebase_init =  new FirebaseInit();
 	firebase_init.is_login(null,"index.html");
-	firebase.auth().onAuthStateChanged(user => 
-	firebase_init.getCurrentTeacherVariable (myFunc,"lesson"));	
+	firebase.database().ref("/user").once('value', function (snapshot) {
+		database = snapshot.val();
+	  }).then(function () {
+		user_uid = firebase.auth().currentUser.uid;
+	  }).then(function () {
+		update_profile(database.client,database.teacher,user_uid)
+		displayContent();
+	  });
+	
+}
+function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+function update_profile(client_db,teacher_db, class_user_uid) {
+	if (client_db[class_user_uid].pic_url.length!=0) {
+		$("#profile_url").attr("src",client_db[class_user_uid].pic_url);
+	}	
+	$("#profile_bday").html(client_db[class_user_uid].bday);
+	$("#profile_age").html(calculateAge(new Date(Date.parse(client_db[class_user_uid].bday))));
+	$("#profile_name").html(client_db[class_user_uid].name);
+	$("#profile_sign_up_date").html(client_db[class_user_uid].create_date_user);
+	$("#profile_email").html(client_db[class_user_uid].email);
+	
+	if("lessons" in teacher_db[class_user_uid]){
+		$("#lessons_opened").html(Object.keys(teacher_db[class_user_uid].lessons).length)
+	}
+	if("my_lessons_list" in client_db[class_user_uid]){
+		$("#lessons_signed_up").html(Object.keys(client_db[class_user_uid].my_lessons_list).length)
+	}
+	const date1 = new Date();
+	const date2 = new Date(Date.parse(client_db[class_user_uid].create_date_user));
+	const diffTime = Math.abs(date2 - date1);
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	$("#user_life").html(diffDays);
 }
 
-function myFunc(lesson) {
-	user_uid = firebase_init.get_user().uid;
-	user_bday = firebase_init.get_user().bday;
-	user = firebase_init.get_user();
-	var d=new Date();
-	console.log(d);
-	d=Date.parse(user_bday);
-	console.log(d);
-	document.getElementById('email-address').innerHTML=user.email;
-	document.getElementById('name').innerHTML=user.name;
-	document.getElementById('bdate').innerHTML='Birthdate: '+user.bday;
-	//document["image"].src = user.pic_url.src;
-	console.log(d);
-	displayContent();
-}
 
 function displayContent(){
   document.getElementById("allContent").style.display = "block";
