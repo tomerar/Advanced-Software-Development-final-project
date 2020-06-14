810089824148c34a5fb9484a224f747adc681b1d
Oren Or <orensmails@gmail.com>
2020-06-14 17:04:45 +0300

    more dynamic data updates

--- js/profile.js
+++ js/profile.js
@@ -1,11 +1,31 @@
-
-$(document).ready(function() {
-  var firebase_init = new FirebaseInit();
-  firebase_init.is_login(null,"login.html")
-  var user = firebase.auth().currentUser;
-  firebase.database().ref("user/client/" + user.uid).once('value').then(function (snapshot) {
-    data_client = snapshot.val();
-  }).then(function () {})
+$(document).ready(function () {
+   initFireBase();  
 });
 
+async function initFireBase(){
+	firebase_init =  new FirebaseInit();
+	firebase_init.is_login(null,"index.html");
+	firebase.auth().onAuthStateChanged(user => 
+	firebase_init.getCurrentTeacherVariable (myFunc,"lesson"));	
+}
 
+function myFunc(lesson) {
+	user_uid = firebase_init.get_user().uid;
+	user_bday = firebase_init.get_user().bday;
+	user = firebase_init.get_user();
+	var d=new Date();
+	console.log(d);
+	d=Date.parse(user_bday);
+	console.log(d);
+	document.getElementById('email-address').innerHTML=user.email;
+	document.getElementById('name').innerHTML=user.name;
+	document.getElementById('bdate').innerHTML='Birthdate: '+user.bday;
+	//document["image"].src = user.pic_url.src;
+	console.log(d);
+	displayContent();
+}
+
+function displayContent(){
+  document.getElementById("allContent").style.display = "block";
+  document.getElementById("loader").style.display = "none";
+}
