6b3ce76987aa48a936b0590099e48e4579716e72
norm723 <norm.twersky@gmail.com>
2020-08-09 22:33:33 +0300

    added more tests

---
+++ cypress/integration/addLesson.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    const TEST_UID = Cypress.env("TEST_UID");
+    beforeEach(() => {
+        cy.login()
+    })
+
+    afterEach(() => {
+        cy.logout();
+        cy.removeLesson();
+    })
+  
+    it('add lesson to teach, should succeed', () => {
+        cy.visit('/index.html')
+        cy.get(':nth-child(7) > .nav-link').click();
+
+        cy.get('#selectSubject').select('Mathematics');
+        cy.get('#selectTitle').type('Calc 1').should('have.value', 'Calc 1');
+        cy.get(':nth-child(4) > .next').click();
+        cy.get('#selctedDate').clear().type('08/19/2021').should('have.value', '08/19/2021');
+        cy.get('#submit').click();
+        cy.get('.col-7 > .purple-text').scrollIntoView().contains('You Have Successfully Signed Up Your Lesson')
+        
+    })
+  })
---
+++ cypress/integration/profile.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    const TEST_UID = Cypress.env("TEST_UID");
+    beforeEach(() => {
+        cy.login()
+    })
+
+    afterEach(() => {
+        cy.logout();
+        cy.callRtdb("set", "/user/teacher/akYmJTGxg0Pg5PRMGwlVmV47kB92/name", 'test');
+    })
+  
+    it('edit profile name, should succeed', () => {
+        cy.visit('/index.html')
+        cy.get(':nth-child(6) > .nav-link').click();
+
+        cy.get('#edit_profile_btn').click();
+        cy.get('#change_name_input').type('tester').should('have.value', 'tester');
+        cy.get('#save_changes_btn').click();
+        cy.get('#profile_name').scrollIntoView().contains('tester')
+        
+    })
+  })
